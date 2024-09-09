d916d446a017ad42b5ee0e71353c3e7945868269
campique <info@campique.com>
2024-09-09 10:19:26 +0200

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,27 +1,26 @@
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
+self.addEventListener('push', function(event) {
+  console.log('Push event received');
+  
+  if (event.data) {
+    console.log('Push event has data:', event.data.text());
+    
+    const data = event.data.json();
+    const options = {
+      body: data.message,
+      icon: 'path/to/icon.png'
+    };
+    
+    event.waitUntil(
+      self.registration.showNotification('Nieuwe melding', options)
+        .then(() => console.log('Notification shown'))
+        .catch(error => console.error('Error showing notification:', error))
+    );
+  } else {
+    console.log('Push event has no data');
   }
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
-self.addEventListener('push', function(event) {
-  const data = event.data.json();
-  const options = {
-    body: data.message,
-    icon: 'https://fcdn.answerly.io/477718fd-e50b-4758-8fcb-7709dedf75ec/29cfa402-2d4f-4a02-85f2-3e02f9242326.png'
-  };
-  event.waitUntil(
-    self.registration.showNotification('Nieuwe melding', options)
-  );
+self.addEventListener('notificationclick', function(event) {
+  console.log('Notification clicked');
+  event.notification.close();
 });
